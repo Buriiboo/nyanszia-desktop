14a719e340b422dba88920db69e76237ba3cd0ca
Buriiboo <Buraian@live.se>
2025-04-07 04:40:44 +0200

    debugging live2dcubismframework.js and live2dcubismcore.min.js

---
+++ assets/cubism/Framework/live2dcubismframework.js
@@ -0,0 +1,8 @@
+
+var Live2DCubismFramework = Live2DCubismFramework || {};
+(function(module) {
+  module.CubismFramework = {
+    startUp: function(option) { console.log("✅ CubismFramework started", option); },
+    initialize: function() { console.log("✅ CubismFramework initialized"); },
+  };
+})(Live2DCubismFramework);
